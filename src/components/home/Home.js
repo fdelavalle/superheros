e5d543fb09fe97e9f1790870adc663b7c08604a5
fdelavalle <delavallef4@gmail.com>
2021-07-29 10:27:00 -0300

    added home text and add superhero icon

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,11 +6,24 @@ const Home = () => {
   const superherosList = [];
 
   return (
-    <section style={{ height: '100vh' }}>
-      <div class='alert alert-primary p-3' role='alert'>
-        <p className='fs-3 text-center text-secondary'>
+    <section
+      style={{ height: '100vh' }}
+      className='d-flex align-items-center justify-content-center'
+    >
+      <div
+        class='d-flex flex-column align-items-center alert alert-primary justify-content-start w-50 h-50 p-3'
+        role='alert'
+      >
+        <p className='fs-3 text-center text-secondary mt-5'>
           Your team is empty, you can start adding superheros here
         </p>
+        <Link className='p-6 text-center'>
+          <img
+            alt='add a hero to the team icon'
+            src='https://icons.iconarchive.com/icons/custom-icon-design/silky-line-user/512/user-add-icon.png'
+            className='w-25'
+          ></img>
+        </Link>
       </div>
 
       {superherosList.length > 0 && (
