b748b8de2646bb4ef677998b29f710762ab48b7a
fdelavalle <delavallef4@gmail.com>
2021-09-09 09:26:59 -0300

    added logintext

--- frontend/src/components/login/LoginText.jsx
+++ frontend/src/components/login/LoginText.jsx
@@ -7,8 +7,7 @@ const LoginText = () => {
         Superheros App
       </h1>
       <p className='p-1 text-center w-75 text-white mx-auto'>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex eligendi
-        quia eaque corrupti magni earum architecto.
+        Assemble your dreamed superhero squad from a pool of heros full of them!
       </p>
     </React.Fragment>
   );
